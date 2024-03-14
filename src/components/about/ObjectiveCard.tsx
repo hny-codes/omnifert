23b6a28c94b49a996ae1855038444845ca46d8e4
Henry <hnguyhenry@protonmail.com>
2024-03-13 18:18:38 -0700

    Add Objective section to about page

---
+++ src/components/about/ObjectiveCard.tsx
@@ -0,0 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+type Props = {
+  title: string;
+  body: string;
+  image: ImageMetadata;
+};
+
+export default function ObjectiveCard({ title, body, image }: Props) {
+  return (
+    <Card className='bg-transparent border-none shadow-none grid grid-cols-[auto_minmax(0,_1fr)] gap-2'>
+      <div className='bg-[--clr-green-02] p-4 w-fit h-fit rounded-xl mt-8'>
+        <img src={image.src} alt={`${title} icon`} />
+      </div>
+      <div className='grid grid-rows-none md:grid-rows-[100px_1fr]'>
+        <CardHeader>
+          <CardTitle className='font-gothamRounded text-[--clr-green-03] text-[1.375rem]'>
+            {title}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className='font-montserrat text-[--clr-font] text-sm leading-loose'>
+            {body}
+          </p>
+        </CardContent>
+      </div>
+    </Card>
+  );
+}
