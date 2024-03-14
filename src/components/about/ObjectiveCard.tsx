07790075c933667f739a6201b95e5334ea29f60c
Henry <hnguyhenry@protonmail.com>
2024-03-13 18:35:55 -0700

    Fix grid template spacing

--- src/components/about/ObjectiveCard.tsx
+++ src/components/about/ObjectiveCard.tsx
@@ -19,7 +19,7 @@ export default function ObjectiveCard({ title, body, image }: Props) {
       <div className='bg-[--clr-green-02] p-4 w-fit h-fit rounded-xl mt-8'>
         <img src={image.src} alt={`${title} icon`} />
       </div>
-      <div className='grid grid-rows-none md:grid-rows-[100px_1fr]'>
+      <div className='grid grid-rows-none md:grid-rows-[125px_1fr] lg:grid-rows-[100px_1fr]'>
         <CardHeader>
           <CardTitle className='font-gothamRounded text-[--clr-green-03] text-[1.375rem]'>
             {title}
