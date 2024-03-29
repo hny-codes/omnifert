93c0411a53819e61779384fbb5fa915c7957b7c5
Henry <hnguyhenry@protonmail.com>
2024-03-28 18:20:32 -0700

    Create cart persisted nanostore

---
+++ src/nanostores/cartStorePersist.ts
@@ -0,0 +1,46 @@
+import { atom, map, computed } from 'nanostores';
+import { persistentMap, persistentAtom } from '@nanostores/persistent';
+
+export type CartItem = {
+  id: string;
+  title: string;
+  image: {
+    src: string;
+    width: number;
+    height: number;
+    format: 'png' | 'jpg' | 'jpeg' | 'tiff' | 'webp' | 'gif' | 'svg' | 'avif';
+  };
+  quantity: number;
+};
+
+export const cartItems = persistentAtom<CartItem[]>('cart', [], {
+  encode: JSON.stringify,
+  decode: JSON.parse,
+});
+
+// Function to add item to Cart
+type ItemDisplay = Pick<CartItem, 'id' | 'image' | 'quantity' | 'title'>;
+
+export function addToCart({ id, title, image, quantity }: ItemDisplay) {
+  const existingItem = cartItems.get().filter((items) => items.id === id);
+
+  // If item exist (array is not 0), update its quantity, else add new cart item
+  if (existingItem.length != 0) {
+    const filterCart = cartItems.get().filter((items) => items.id !== id);
+    const updatedQuantity = existingItem[0].quantity + quantity;
+    cartItems.set([
+      ...filterCart,
+      { id, title, image, quantity: updatedQuantity },
+    ]);
+  } else if (quantity > 0) {
+    cartItems.set([...cartItems.get(), { id, title, image, quantity }]);
+  }
+}
+
+export function getStore() {
+  return cartItems.get();
+}
+
+export const cartQuantity = computed(cartItems, (items) => {
+  return items.reduce((total, item) => total + item.quantity, 0);
+});
