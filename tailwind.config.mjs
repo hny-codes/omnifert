725353c570a7693cb88c587fe76e2d7332d95ee3
Henry <hnguyhenry@protonmail.com>
2024-03-29 20:35:31 -0700

    Adjust lg media query

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,7 +15,7 @@ module.exports = {
       md: '710px',
       // => @media (min-width: 768px) { ... }
 
-      lg: '1024px',
+      lg: '1034px',
       // => @media (min-width: 1024px) { ... }
 
       xl: '1280px',
