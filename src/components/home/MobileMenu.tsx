5912828d46ab76537bd5db267b376fecd37df27a
Henry <hnguyhenry@protonmail.com>
2024-04-19 19:22:47 -0700

    Wrap and replace contact info within Popover component

--- src/components/home/MobileMenu.tsx
+++ src/components/home/MobileMenu.tsx
@@ -1,8 +1,8 @@
 import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from '@/components/ui/hover-card';
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 
 import Location from '../../assets/home/location.png';
 import Email from '../../assets/home/email.png';
@@ -13,11 +13,9 @@ import LinkedIn from '../../assets/home/linkedin.png';
 export default function MobileMenu() {
   return (
     <aside className='font-montserrat text-[--clr-white-01] flex justify-between py-2'>
-      <HoverCard>
-        <HoverCardTrigger className='hover:cursor-pointer lg:hidden'>
-          @Contact
-        </HoverCardTrigger>
-        <HoverCardContent className='[&>*]:mb-4 bg-[--clr-green-01] text-[--clr-white-01]'>
+      <Popover>
+        <PopoverTrigger className='lg:hidden'>@Contact</PopoverTrigger>
+        <PopoverContent className='[&>*]:mb-4 bg-[--clr-green-01] text-[--clr-white-01] border-none mt-2 ml-4'>
           <div className='flex gap-4 items-center'>
             <img src={Location.src} alt='' />
             <p>No.28 Church Cres St, North Labone, Accra, Ghana</p>
@@ -33,8 +31,9 @@ export default function MobileMenu() {
             <img src={Phone.src} alt='' />
             <p>+233 302 767 400</p>
           </a>
-        </HoverCardContent>
-      </HoverCard>
+        </PopoverContent>
+      </Popover>
+
       <div className='hidden lg:flex gap-8'>
         <div className='flex gap-2 items-center'>
           <img src={Location.src} alt='' />
